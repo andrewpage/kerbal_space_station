9e56b3c856620056fc1d04afe550d4d65b349326
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-05-02 22:11:10 -0700

    This makes requests take less long

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,7 @@ CarrierWave.configure do |config|
     provider: "AWS",
     aws_access_key_id: ENV["AMAZON_S3_KEY"],
     aws_secret_access_key: ENV["AMAZON_S3_SECRET"],
+    region: "us-west-2"
   }
   config.fog_directory  = ENV["AMAZON_BUCKET"]
   config.fog_public     = true
