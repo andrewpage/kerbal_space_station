a6b6ba21e1959a3ce87b6840bb84db22faf0fa71
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-05-02 22:10:49 -0700

    We don't need this for the initial setup

--- app/models/mod.rb
+++ app/models/mod.rb
@@ -1,7 +1,6 @@
 class Mod < Downloadable
-
   validates :install, presence: true
-  validates :changelog, presence: true
+  validates :changelog, presence: true, on: :update
 
   attr_accessible :changelog
   attr_accessible :install
