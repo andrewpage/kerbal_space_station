afd2e64492bab2a5b40f4688fec2bc1ca45f6b9d
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-05-03 03:37:55 -0700

    Fancy new button coloring

--- app/decorators/mod_decorator.rb
+++ app/decorators/mod_decorator.rb
@@ -1,6 +1,18 @@
 class ModDecorator < Draper::Decorator
   delegate_all
 
+  def liked_style
+    "btn-success" if h.current_account.likes?(source)
+  end
+
+  def disliked_style
+    "btn-danger" if h.current_account.dislikes?(source)
+  end
+
+  def bookmarked_style
+    "btn-info" if h.current_account.bookmarks?(source)
+  end
+
   def primary_image
     images.first.image
   end
