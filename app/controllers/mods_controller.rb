8ab81c6ed20404e61298734b63eaed1d5fb4bde8
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-05-03 03:36:00 -0700

    WE'd like to be able to do various things to mods

--- app/controllers/mods_controller.rb
+++ app/controllers/mods_controller.rb
@@ -1,5 +1,6 @@
 class ModsController < ApplicationController
   before_filter :authenticate_account!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :find_mod, only: [:show, :ignore, :bookmark, :report, :like, :dislike, :download]
 
   VALID_MOD_KEYS = [:name, :upload, :images_attributes, :install, :description, :changelog, :version, :compatible]
 
@@ -8,7 +9,7 @@ class ModsController < ApplicationController
   end
 
   def show
-    @mod = Mod.find(params[:id]).decorate
+    @mod = @mod.decorate
   end
 
   def new
@@ -39,6 +40,7 @@ class ModsController < ApplicationController
     @mod.assign_attributes(params[:mod].slice(*VALID_MOD_KEYS))
     if @mod.valid?
       @mod.save!
+      flash[:notice] = "Your mod has been saved!"
       redirect_to(@mod)
     else
       render(:edit)
@@ -49,5 +51,54 @@ class ModsController < ApplicationController
     @mod = current_account.mods.find(params[:id])
     @mod.destroy
     redirect :back
+  end
+
+  def like
+    unless current_account.likes?(@mod)
+      current_account.like(@mod)
+    else
+      current_account.unlike(@mod)
+    end
+    flash[:notice] = "You've liked #{@mod.name}."
+    redirect_to :back
+  end
+
+  def dislike
+    unless current_account.dislikes?(@mod)
+      current_account.dislike(@mod)
+    else
+      current_account.undislike(@mod)
+    end
+    flash[:notice] = "You've disliked #{@mod.name}."
+    redirect_to :back
+  end
+
+  # def report
+  #   @mod.reported_by(current_account)
+  #   flash.now[:notice] = "Thanks for reporting this upload! We'll check it out."
+  #   redirect_to :back
+  # end
+
+  def bookmark
+    unless current_account.bookmarks?(@mod)
+      current_account.bookmark(@mod)
+      current_account.increment!(:bookmark_count)
+    else
+      current_account.unbookmark(@mod)
+      current_account.decrement!(:bookmark_count)
+    end
+    flash[:notice] = "You've bookmarked #{@mod.name}."
+    redirect_to :back
+  end
+
+  def download
+    @mod.increment!(:download_count)
+    redirect_to @mod.upload
+  end
+
+  private
+
+  def find_mod
+    @mod = Mod.find(params[:id])
   end
 end
