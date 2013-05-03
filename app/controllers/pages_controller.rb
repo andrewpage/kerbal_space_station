f135c03531f38256fc03903b6bc01cd356275dd6
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-05-03 04:03:32 -0700

    Wouldn't work if the top 3 showed up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def splash
     Account
     @view_full = true
-    @top_mods = if Mod.top(3).any? then Mod.top(3) else Mod.limit(3) end.decorate
+    mods = if Mod.top(3).any? then Mod.top(3) else Mod.limit(3) end
+    @top_mods = ModDecorator.decorate_collection(mods)
   end
 end
