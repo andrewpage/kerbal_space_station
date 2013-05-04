7bdc86e5af66eade60506ce35564a40fadcc96ad
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-05-03 21:09:46 -0700

    This was way too big, with comments we don't need, and wasn't sizing the way we want

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,44 +1,23 @@
 class ImageUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
-  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
   include Sprockets::Helpers::RailsHelper
   include Sprockets::Helpers::IsolatedHelper
 
-  # Including optimizing features
-  # include CarrierWave::ImageOptimizer
-
-  # Choose what kind of storage to use for this uploader:
-  # storage :file
   storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  version :carousel do
+    process :resize_to_fit => [800, 600]
+  end
 
-  # Process files as they are uploaded:
-  process :resize_to_fit => [800, 600]
-  # process :optimize
-
-  # Create different versions of your uploaded files:
   version :medium do
-    process :resize_to_fit => [300, 300]
+    process :resize_to_fill => [300, 300]
   end
 
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_fill => [50, 50]
   end
 end
