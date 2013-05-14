41ef802c0bbce77525adb68e23b21381ddb36640
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-05-14 15:31:24 -0700

    This is a mod specific attribute

--- app/models/craft.rb
+++ app/models/craft.rb
@@ -1,5 +1,6 @@
 class Craft < Downloadable
   VALID_PARAMS = [:name, :file, :images_attributes, :description, :compatible, :tags]
   validates :file, presence: true, on: :create
+
   attr_accessible :file
 end
