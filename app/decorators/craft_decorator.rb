3316e3756ab43e1bf46ca3c7b0bea7b2353f9b4b
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-05-14 15:30:57 -0700

    It's more sustainable to put these here

--- app/decorators/craft_decorator.rb
+++ app/decorators/craft_decorator.rb
@@ -1,4 +1,14 @@
 class CraftDecorator < DownloadableDecorator
   delegate_all
 
+  def filepicker_options
+    {
+      extension: "*.craft",
+      services: ["COMPUTER"],
+      button_text: "Upload Craft",
+      drag_text: "Drop your craft file here.",
+      button_class: "btn btn-success",
+      multiple: false
+    }
+  end
 end
--- app/decorators/mod_decorator.rb
+++ app/decorators/mod_decorator.rb
@@ -1,4 +1,15 @@
 class ModDecorator < DownloadableDecorator
   delegate_all
 
+  def filepicker_options
+    {
+      mimetypes: ["application/zip"],
+      services: ["COMPUTER"],
+      button_text: "Upload Zip",
+      drag_text: "Drop your zip file here.",
+      button_class: "btn btn-success",
+      multiple: false
+    }
+  end
+
 end
