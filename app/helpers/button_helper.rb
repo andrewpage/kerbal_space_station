bbcbc4f33b516289c4c83f133c69760c857e4c2d
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-05-18 04:15:56 -0700

    This should allow all special buttons to default to text

--- app/helpers/button_helper.rb
+++ app/helpers/button_helper.rb
@@ -3,12 +3,12 @@ module ButtonHelper
     link_to raw(text), path, options.merge!(class: "btn #{options[:class]}")
   end
 
-  def download_button
-    buttoner("download", "Download")
+  def download_button(has_text = true)
+    buttoner("download", "Download", has_text)
   end
 
-  def upload_button
-    buttoner("upload", "Upload")
+  def upload_button(has_text = true)
+    buttoner("upload", "Upload", has_text)
   end
 
   def version_button(mod)
@@ -19,31 +19,35 @@ module ButtonHelper
     "KSP #{mod.compatible} #{caret_tag}"
   end
 
-  def like_button
-    buttoner("thumbs-up", "Like")
+  def like_button(has_text = true)
+    buttoner("thumbs-up", "Like", has_text)
   end
 
-  def dislike_button
-    buttoner("thumbs-down", "Dislike")
+  def report_button(has_text = true)
+    buttoner("flag", "Report", has_text)
   end
 
-  def bookmark_button
-    buttoner("bookmark", "Bookmark")
+  def dislike_button(has_text = true)
+    buttoner("thumbs-down", "Dislike", has_text)
   end
 
-  def delete_button(have_text = true)
-    buttoner("trash", "Delete", have_text)
+  def bookmark_button(has_text = true)
+    buttoner("bookmark", "Bookmark", has_text)
   end
 
-  def edit_button
-    buttoner("edit", "Change")
+  def delete_button(has_text = true)
+    buttoner("trash", "Delete", has_text)
+  end
+
+  def edit_button(has_text = true)
+    buttoner("edit", "Change", has_text)
   end
 
   def close_button(type)
     content_tag :button, icon_tag("remove-sign"), class: "close", data: dismiss(type)
   end
 
-  def buttoner(icon, text = nil, have_text = true)
-    raw(have_text ? "#{icon_tag(icon)} #{text}" : icon_tag(icon))
+  def buttoner(icon, text = nil, has_text = true)
+    raw(has_text ? "#{icon_tag(icon)} #{text}" : icon_tag(icon))
   end
 end
